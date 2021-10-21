fc6eaee0e71c3986161999b5054c9c3304a58458
Magno Rafael <89099521+magrafa@users.noreply.github.com>
2021-10-21 11:36:40 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,12 @@ navLink.forEach((n) => n.addEventListener("click", linkAction));
 /*=============== HOME SWIPER ===============*/
 // cancel
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+function scrollHeader(){
+  const header = document.getElementById('header')
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 /*=============== NEW SWIPER ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
